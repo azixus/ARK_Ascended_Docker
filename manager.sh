01cbf0111978272ae10d937cff835e8287c570ef
Robin Müller <33422394+AziXus@users.noreply.github.com>
2023-11-05 00:39:04 +0100

    Added docker exec manager wrapper

---
+++ manager.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Simple wrapper for docker commands to make life easier. 
+sudo_cmd=( sudo )
+if [[ "$EUID" -eq 0 ]] || id -nGz "$USER" | grep -qzxF "docker"; then
+	sudo_cmd=()
+fi
+
+# Get the container id first. This is somewhat necessary since the next command
+# does not use docker compose.
+container=$($sudo_cmd docker compose ps -q)
+
+# Use docker exec, instead of docker compose exec as the latter does not
+# override env variables.
+$sudo_cmd docker exec --env-file .env "$container" manager "${@}"
