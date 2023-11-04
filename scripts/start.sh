7e1f1cb76f91c0e6bb3315c97182c65a720aaad0
Robin Müller <33422394+AziXus@users.noreply.github.com>
2023-11-04 13:15:14 +0100

    Added flag to remove unnecessary server files

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,6 +9,12 @@ mkdir -p /home/arkuser/.steam/steam/steamapps/compatdata/${ASA_APPID}
 # Install or update ASA server + verify installation
 /opt/steamcmd/steamcmd.sh +force_install_dir /opt/arkserver +login anonymous +app_update ${ASA_APPID} validate +quit
 
+# Remove unnecessary files (saves 6.4GB.., that will be re-downloaded next update)
+if [ -n ${REDUCE_IMAGE_SIZE} ]; then 
+    rm -rf /opt/arkserver/ShooterGame/Binaries/Win64/ArkAscendedServer.pdb
+    rm -rf /opt/arkserver/ShooterGame/Content/Movies/
+fi
+
 #Create file for showing server logs
 mkdir -p /opt/arkserver/ShooterGame/Saved/Logs && touch /opt/arkserver/ShooterGame/Saved/Logs/ShooterGame.log
 
