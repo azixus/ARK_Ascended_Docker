a85a9cba087c1a1de5273a71aa6157f6ac5e3538
Niklas <niklas.kinze@gmail.com>
2023-11-10 19:49:14 +0100

    changed tar mecanism

--- scripts/manager/manager_backup.sh
+++ scripts/manager/manager_backup.sh
@@ -2,12 +2,22 @@
 set -e
 # create backup folder if it not already exists
 path="/var/backups/asa-server"
+copyPath="/opt/arkserver/tmp/backup"
+
 mkdir -p $path
+mkdir -p $copyPath
 
 archive_name=$(date +"%Y-%m-%d_%H-%M-%S")
 
-tar -czf $path/backup_${archive_name}.tar.gz -C /opt/arkserver/ShooterGame Saved
+# copy live path to another folder so tar doesnt get any write on read fails
+echo "copying save folder"
+cp -r -R /opt/arkserver/ShooterGame/Saved $copyPath
 
+# tar.gz from the copy path
+echo "creating archive"
+tar -cvzf $path/backup_${archive_name}.tar.gz -C $copyPath Saved
+
+rm -R $copyPath/*
 # count and output existing backups
 
 count=$(ls $path | wc -l)
