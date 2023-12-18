fcfdf99924ae0366475a8fbec31c19a604726a14
Robin Müller <33422394+AziXus@users.noreply.github.com>
2023-12-18 20:55:37 +0100

    Fixed sudo_cmd variable access in manager.sh

--- manager.sh
+++ manager.sh
@@ -11,8 +11,8 @@ fi
 
 # Get the container id first. This is somewhat necessary since the next command
 # does not use docker compose.
-container=$($sudo_cmd docker compose -f "$script_dir/docker-compose.yml" ps -q asa_server)
+container=$(${sudo_cmd[@]} docker compose -f "$script_dir/docker-compose.yml" ps -q asa_server)
 
 # Use docker exec, instead of docker compose exec as the latter does not
 # override env variables.
-$sudo_cmd docker exec -it --env-file "$script_dir/.env" "$container" manager "${@}"
+${sudo_cmd[@]} docker exec -it --env-file "$script_dir/.env" "$container" manager "${@}"
